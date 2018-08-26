5e728898f0532a7ec8923a6d153290e81365b0d9
Rudi Kelly <rudikellyy@gmail.com>
2018-08-26 10:07:27 +0100

    Beautify help commands

--- jb-bot.py
+++ jb-bot.py
@@ -553,56 +553,57 @@ async def help(ctx):
         await msg.add_reaction(u"\u27A1")
 
 
-@help.command(aliases=['jb', 'canijailbreak'])
-async def canijb(ctx):
+
+@help.command(aliases=['jb', 'canijailbreak'], name="canijb")
+async def canijb_help(ctx):
     embed = discord.Embed(title="Jailbreak Bot Help", color=embed_color)
     embed.add_field(name="$canijb [ios]  or  $jb [ios]", value="Check if the given iOS is jailbreak-able. Works for every ios except 5, not sure . Sends name of the jailbreak tool and link to get it", inline=False)
     embed.add_field(name="Examples", value="$canijb 11.0\n$canijb ios 6.1.3\n$jb 7.1.2\n$jb ios 5.1", inline=False)
     await ctx.send(embed=embed)
 
 
-@help.command(aliases=['tvos'])
-async def profile(ctx):
+@help.command(aliases=['tvos'], name="profile")
+async def profile_help(ctx):
     embed = discord.Embed(title="Jailbreak Bot Help", color=embed_color)
     embed.add_field(name="$profile  or  $tvos", value="Sends the tvOS 11 beta profile in the current channel, allowing you to 1-click install it", inline=True)
     embed.add_field(name="Examples", value="$profile\n$tvos", inline=False)
     await ctx.send(embed=embed)
 
 
-@help.command(aliases=['theme'])
-async def tweak(ctx):
+@help.command(aliases=['theme'], name="tweak")
+async def tweak_help(ctx):
     embed = discord.Embed(title="Jailbreak Bot Help", color=embed_color)
     embed.add_field(name="$tweak [tweak]  or  $theme [theme]", value="Provides information about and a download link for a specific tweak. Currently only works for defualt repos", inline=True)
     embed.add_field(name="Examples", value="$tweak icleaner\n$tweak classicfolders 2\n$theme indigo", inline=False)
     await ctx.send(embed=embed)
 
 
-@help.command(aliases=['doc'])
-async def docs(ctx):
+@help.command(aliases=['doc'], name="docs")
+async def docs_help(ctx):
     embed = discord.Embed(title="Jailbreak Bot Help", color=embed_color)
     embed.add_field(name="$docs [object]  or  $doc [data type]  etc.", value="Provides a link to requested Apple Obj-C doc, as well as a short summary. Only frameworks currently supported are: Objective-C, UIKit, WebKit, Foundation, CoreGraphics, CoreData, Kernel, CoreServices", inline=True)
     embed.add_field(name="Examples", value="$docs NSObject\n$doc UIViewController\n$docs CGFloat", inline=False)
     await ctx.send(embed=embed)
 
 
-@help.command(aliases=['h'])
-async def header(ctx):
+@help.command(aliases=['h'], name="header")
+async def header_help(ctx):
     embed = discord.Embed(title="Jailbreak Bot Help", color=embed_color)
     embed.add_field(name="$header [header] <ios> <framework>  or  $h [header] <framework> <ios>", value="Provides a link to requested header file. Case Sensitive. Generates a link to developer.limneos.net. Only frameworks currently supported are: SpringBoard, UIKit, WebKit, Foundation, CoreData, CoreServices. However it will check any frameworks provided explicitly", inline=True)
     embed.add_field(name="Examples", value="$header SBAlertView\n$h SBPowerDownAlertView 10.2\n$h CXStartCallAction.h CallKit 10.1.1", inline=False)
     await ctx.send(embed=embed)
 
 
-@help.command(aliases=['f'])
-async def framework(ctx):
+@help.command(aliases=['f'], name="framework")
+async def framework_help(ctx):
     embed = discord.Embed(title="Jailbreak Bot Help", color=embed_color)
     embed.add_field(name="$framework [framework]  or  $f [framework]", value="Provides a link to requested framework on developer.limneos.net. Case Sensitive.", inline=True)
     embed.add_field(name="Examples", value="$framework SpringBoard\n$f UIKit\n$f Foundation", inline=False)
     await ctx.send(embed=embed)
 
 
-@help.command()
-async def xkcd(ctx):
+@help.command(name="xkcd")
+async def xkcd_help(ctx):
     embed = discord.Embed(title="Jailbreak Bot Help", color=embed_color)
     embed.add_field(name="$xkcd random  or  $xkcd latest  or  $xkcd [number]", value="Sends the requested xkcd comic in the current channel. Defaults to random", inline=True)
     embed.add_field(name="Examples", value="$xkcd latest\n$xkcd 678\n$xkcd", inline=False)
