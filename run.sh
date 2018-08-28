8982195f39a652da535de480c8286573fe2f4b51
Rudi Kelly <rudikellyy@gmail.com>
2018-08-28 02:27:11 +0100

    make run.sh only run bot

--- run.sh
+++ run.sh
@@ -1,25 +1,3 @@
 #!/bin/sh
 
-# Checks for updates via github
-update() {
-  UPSTREAM=${1:-'@{u}'}
-  LOCAL=$(git rev-parse @)
-  REMOTE=$(git rev-parse "$UPSTREAM")
-  BASE=$(git merge-base @ "$UPSTREAM")
-
-  if [ $LOCAL = $REMOTE ]; then
-    echo "Bot is up-to-date"
-  elif [ $LOCAL = $BASE ]; then
-    echo "Updating bot..."
-    git fetch
-    git pull
-  fi
-}
-
-# Runs bot
-run() {
-  pipenv run python jb-bot.py
-}
-
-update
-run
+pipenv run python jb-bot.py
