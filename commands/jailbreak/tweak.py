4c57167a6932689746c53fee92b0b66834945b6c
Rudi Kelly <rudikellyy@gmail.com>
2018-09-02 01:04:46 +0100

    Add preliminary support for 3rd party repos

--- commands/jailbreak/tweak.py
+++ commands/jailbreak/tweak.py
@@ -4,6 +4,18 @@ from discord.ext import commands
 from bs4 import BeautifulSoup
 import json
 import aiohttp
+import config.loadconfig as cfg
+import logging as log
+
+repo_list = []
+for repo in cfg.repo_list:
+    repo_list.append(repo)
+try:
+    with open("config/servers.json", 'r') as f:
+        server_repo_list = json.load(f)["servers"]
+
+except FileNotFoundError:
+    log.error("server.json not found")
 
 
 class tweak():
@@ -11,25 +23,27 @@ class tweak():
     def __init__(self, bot):
         self.bot = bot
 
-    async def __error(self, ctx, error):
-        await ctx.send("Usage:")
-        await ctx.send("``$tweak [tweak]``")
-        print("$tweak errored:")
-        print(error)
+    # async def __error(self, ctx, error):
+    #     await ctx.send("Usage:")
+    #     await ctx.send("``$tweak [tweak]``")
+    #     print("$tweak errored:")
+    #     print(error)
 
     @commands.command(aliases=['tweakinfo', 'theme'], usage='[tweak]')
-    async def tweak(self, ctx, *, tweak: str):
+    async def tweak(self, ctx, *, query: str):
+
+        tweak = query.replace(' ', '').lower().strip()
 
         # grabs data about tweak from sauriks api
         async with aiohttp.ClientSession() as session:
-            async with session.get("https://cydia.saurik.com/api/macciti?query=" + tweak.replace(' ', '').lower().strip()) as r:
+            async with session.get("https://cydia.saurik.com/api/macciti?query=" + tweak) as r:
                 t = await r.text()
                 data = json.loads(t)
 
                 # checks if the tweak matches the api's first response
                 for x in range(len(data["results"])):
                     rtweak = data["results"][x]
-                    if (rtweak["display"].replace(' ', '').lower().strip() == tweak.replace(' ', '').lower().strip()):
+                    if (rtweak["display"].replace(' ', '').lower().strip() == tweak):
 
                         # gathers basic info
                         tweakname = rtweak["display"]
@@ -68,8 +82,49 @@ class tweak():
                         await ctx.send(embed=embed)
                         return
 
-                # if command fails
-                await ctx.send("Couldn't find info for tweak " + tweak)
+            for repo in server_repo_list[str(ctx.message.guild.id)]["repo_list"]:
+                repo_list.append(repo)
+
+            exact_match = False
+
+            # if tweak isnt found on a default repo, search non-default ones
+            for repo in repo_list:
+                # searches as though the query is a tweak name
+                async with session.get("https://cydia.s0n1c.org/cydia/" + "?q=" + tweak + "&url=" + repo) as resp:
+                    if resp.status == 200:
+                        data = json.loads(await resp.text())
+                    else:
+                        await ctx.send("Error contacting cydia :(")
+                        log.error("Got non-200 status code from API")
+                if data["status"]:
+                    for x in range(len(data["results"])):
+                        result = data["results"][str(list(data["results"].keys())[x])]
+                        if result["name"].strip().lower() == tweak:
+                            tweak_data = result
+                            exact_match = True
+                            break
+                    if not exact_match:
+                        tweak_data = data["results"][str(list(data["results"].keys())[x])]
+                    async with session.get("https://cydia.s0n1c.org/cydia/" + "?url=" + repo) as resp:
+                        repo_info = json.loads(await resp.text())["info"]
+                        repo_icon_url = repo_info["icon"]
+                        repo_name = repo_info["Label"]
+
+                    # constructs nice embed
+                    embed = discord.Embed(title=tweak_data["name"], url=tweak_data["depict"], color=discord.Colour(0x96c8fa))
+                    embed.set_footer(text=tweak_data["name"] + " - " + repo_name, icon_url=repo_icon_url)
+                    embed.add_field(name="Section", value=tweak_data["section"], inline=True)
+                    embed.add_field(name="Repo", value=repo, inline=True)
+                    if not tweak_data["author"] is "":
+                        embed.add_field(name="Author", value=tweak_data["author"], inline=True)
+                    embed.add_field(name="Summary", value=tweak_data["desc"], inline=False)
+                    if not exact_match:
+                        await ctx.send("Couldn't find an exact match :(. This might be it")
+                    await ctx.send(embed=embed)
+                    return
+
+            else:
+                await ctx.send("Couldn't find info for tweak " + query)
 
 
 def setup(bot):
