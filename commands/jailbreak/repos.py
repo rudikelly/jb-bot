eae388c9a1ff91625098e1234004ac1e1796f14d
Rudi Kelly <rudikellyy@gmail.com>
2018-09-03 20:26:05 +0100

    implement 'repo add' and 'repo remove'

--- commands/jailbreak/repos.py
+++ commands/jailbreak/repos.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import aiohttp
 import config.loadconfig as cfg
 
 
@@ -19,7 +20,7 @@ class repos():
     async def repos(self, ctx):
         builtin_repo_list = ""
         for repo in cfg.repo_list:
-            builtin_repo_list += repo + "\n"
+            builtin_repo_list += repo + " \n"
         server_repo_list = ""
         with open("config/servers.json", 'r+') as f:
             servers = json.load(f)["servers"]
@@ -32,9 +33,46 @@ class repos():
         embed.add_field(name="Server Repos:", value=server_repo_list, inline=True)
         await ctx.send(embed=embed)
 
-    @commands.command()
-    async def repo():
-        pass
+    @commands.group()
+    async def repo(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("Usage: `repo add [repo]`  or  `repo remove [repo]`")
+
+    @repo.command()
+    async def add(self, ctx, repo_to_add):
+        with open("config/servers.json", 'r+') as f:
+            servers = json.load(f)
+            for repo in servers["servers"][str(ctx.message.guild.id)]["repo_list"]:
+                if repo_to_add == repo:
+                    return await ctx.send("That repo has already been added")
+            for repo in cfg.repo_list:
+                if repo_to_add == repo:
+                    return await ctx.send("That repo is a default repo")
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://cydia.s0n1c.org/cydia/" + "?url=" + repo_to_add) as resp:
+                    if resp.status == 200 and json.loads(await resp.text())["status"]:
+                        servers["servers"][str(ctx.message.guild.id)]["repo_list"].append(repo_to_add)
+                        f.seek(0)
+                        json.dump(servers, f, indent=4)
+                        return await ctx.send("Repo succesfully added")
+                    else:
+                        print("repo not found")
+
+    @repo.command()
+    async def remove(self, ctx, repo_to_remove):
+        with open("config/servers.json", 'r') as f:
+            servers = json.load(f)
+        with open("config/servers.json", 'w') as f:
+            for repo in servers["servers"][str(ctx.message.guild.id)]["repo_list"]:
+                if repo_to_remove == repo:
+                    servers["servers"][str(ctx.message.guild.id)]["repo_list"].remove(repo)
+            for repo in cfg.repo_list:
+                if repo_to_remove == repo:
+                    return await ctx.send("That repo is a default repo and can't be removed")
+            print(servers)
+            f.seek(0)
+            json.dump(servers, f, indent=4)
+            return await ctx.send("Repo successfully removed")
 
 
 def setup(bot):
