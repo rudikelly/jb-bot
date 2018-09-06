def581352d1c87effd44862d49ab956dccae63a8
Rudi Kelly <rudikellyy@gmail.com>
2018-09-06 21:15:08 +0100

    improve canijb

--- commands/jailbreak/canijb.py
+++ commands/jailbreak/canijb.py
@@ -19,21 +19,15 @@ class canijb():
 
     @commands.command(aliases=['canijailbreak', 'jb'], usage='[ios]')
     async def canijb(self, ctx, ios: str, ios2: str = ""):
-        print(self)
         await ctx.trigger_typing()
-
         if (ios.lower() == "ios"):
             ios = ios2
 
         # fetches json api, loads into list
         async with aiohttp.ClientSession() as session:
             async with session.get("http://canijailbreak.com/jailbreaks.json") as r:
-                t = await r.text()
-                data = json.loads(t)
+                data = json.loads(await r.text())
                 jailbreaks = data["jailbreaks"]
-
-                jailbroken = False
-                done = False
 
                 for x in range(len(jailbreaks)):
 
@@ -41,9 +35,7 @@ class canijb():
                     if (version.parse(str(jailbreaks[x]["ios"]["start"])) <= version.parse(ios)) and (version.parse(str(jailbreaks[x]["ios"]["end"])) >= version.parse(ios)):
 
                         # checks if it can be jailbroken
-                        jailbroken = jailbreaks[x]["jailbroken"]
-                        if (jailbroken):
-                            done = True
+                        if (jailbreaks[x]["jailbroken"]):
 
                             # grabs name of the tool and the url from the api
                             url = jailbreaks[x]["url"]
@@ -53,16 +45,10 @@ class canijb():
                             embed = discord.Embed(title="iOS " + ios + " can be jailbroken!", description="", color=discord.Colour(0x96c8fa), url="https://canijailbreak.com/")
                             embed.add_field(name="Use the tool " + name + " which you can get at:", value=url, inline=False)
                             # embed.set_footer(icon_url=ctx.message.author.avatar_url_as(), text="Requested by " + str(ctx.message.author))
-                            await ctx.send(embed=embed)
-                            break
+                            return await ctx.send(embed=embed)
                         else:
-                            await ctx.send("iOS " + ios + " can't be jailbroken!")
-                            done = True
-                            break
-                    else:
-                        continue
-                if (not jailbroken and not done):
-                    await ctx.send("Couldn't find data for iOS " + ios)
+                            return await ctx.send("iOS " + ios + " can't be jailbroken!")
+                return await ctx.send("Couldn't find data for iOS " + ios)
 
 
 def setup(bot):
